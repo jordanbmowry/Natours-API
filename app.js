a3bf71a020af3ff6b6eadedaee8aafee44815c56
Jordan Mowry <jordan.mowry@gmail.com>
2022-02-11 16:54:07 -0700

    added  GET /api/v1/tours

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+//"type": "module", in package.json allows es6 modules syntax
 import express from 'express';
+import fs from 'fs';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello world');
+const tours = JSON.parse(fs.readFileSync(`./dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
 export default app;
