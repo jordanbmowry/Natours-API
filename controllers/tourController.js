622cd1209e0172c37cded5c937418f5b78d01b86
Jordan Mowry <jordan.mowry@gmail.com>
2022-02-16 11:35:24 -0700

    added a query in tour controller file

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,7 +4,20 @@ import Tour from '../models/tourModel.js';
 
 const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Build query
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((field) => {
+      delete queryObj[field];
+    });
+    // Execute query
+    const tours = await Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
 
     res.status(200).json({
       status: 'success',
